d14a315c59357b08728959fff3bc2da5c9348dc7
Max <maxi.camps@gmail.com>
2021-08-17 14:27:03 +0200

    v0.02
    sand and water kinda working need to check how to update correctly

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
 var canvasWidth = 800;
 var canvasHeight = 800;
-var dpi = 5;
+var dpi = 10;
+var grid = 0;
+var tmp = 0;
 var Type = {
   sand: 'sand',
   water: 'water',
   rock: 'rock',
   void: 'void',
-  current: 'sand'
+  current: 'sand', 
 }
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
   noStroke();
-  fill(255, 196, 0); // color = orange
-  grid = newArray();
+  newArray();
 }
 
 function draw() {
@@ -21,29 +22,27 @@ function draw() {
   fill(150);
   rect(dpi, dpi, canvasWidth - (2*dpi), canvasHeight - (2*dpi));
   pos = createVector(pmouseX - pmouseX % dpi, pmouseY - pmouseY % dpi);
-  if (pos.x < 0)
-	pos.x = 0;
   click();
-  fill(255, 196, 0);
-  drawSim();
   revSim();
-  update();
+  drawSim();
+  updateUI();
 }
+
 
 function click(){
   if (mouseIsPressed){
-    if (mouseButton === LEFT && pos.x < canvasWidth - dpi&& pos.y < canvasHeight - dpi && pos.x > 0 && pos.y > 0){
-      grid[pos.x / dpi][pos.y / dpi] = Type.current;
+    if (mouseButton === LEFT && pos.x < canvasWidth - dpi && pos.y < canvasHeight - dpi && pos.x > 0 && pos.y > 0){
+      grid[pos.x/dpi][pos.y/dpi] = Type.current;
     }
   }
 }
 
 function sim(){
-  for (let i = 0; i < canvasWidth / dpi; i++){
-    for (let j = 0; j < canvasHeight / dpi; j++){
-      if (grid[i][j] != Type.void){
-		    sand(i, j);
-      }
+  for (let i = 0; i < canvasWidth / dpi - 1; i++){
+    for (let j = 0; j < canvasHeight / dpi - 2; j++){
+      if (grid[i][j] == Type.sand){sand(i, j)}
+		  if (grid[i][j] == Type.rock){rock(i, j)}
+      if (grid[i][j] == Type.water){water(i, j)}
     }
   }
 }
@@ -51,63 +50,67 @@ function sim(){
 function revSim(){
   for (let i = canvasWidth / dpi - 2; i > 0; i--){
     for (let j = canvasHeight / dpi - 3; j > 0; j--){
-      if (grid[i][j] != Type.void){
-        if (grid[i][j] == Type.sand){sand(i, j);}
-		    if (grid[i][j] == Type.rock){rock(i, j)};
-      }
+      if (grid[i][j] == Type.sand){sand(i, j)}
+		  if (grid[i][j] == Type.rock){rock(i, j)}
+      if (grid[i][j] == Type.water){water(i, j)}
     }
   }
 }
 
+// function copyArray(src){
+//   dest = new Array(canvasWidth / dpi);
+//   for (let i = 0; i < canvasWidth / dpi; i++)
+//     dest[i] = src[i].slice();
+//   return dest;
+// }
+
 function rock(i, j){
-	// swapParticle(i, j, 0, 0);
   return  ;
 }
 
 function sand(i, j){
-  if (grid[i][j+1] == Type.void){swapParticle(i, j, 0, 1)}
-  else if (grid[i][j+1] != Type.void && boundary(i, j) == 1){
-    if (grid[i-1][j+1] == Type.void && i != 1){swapParticle(i, j, -1, 1)}
-    else if (grid[i+1][j+1] == Type.void && i != canvasWidth / dpi - 2){swapParticle(i, j, 1, 1)}
-  }
+  if (grid[i][j+1] == Type.void){swapParticle(i, j, 0, 1, Type.sand, Type.void)}
+  else if (grid[i][j+1] == Type.water){swapParticle(i, j, 0, 1, Type.sand, Type.water)}
+  else if (grid[i-1][j+1] == Type.void && i != 1){swapParticle(i, j, -1, 1, Type.sand, Type.void)}
+  else if (grid[i+1][j+1] == Type.void && i != canvasWidth / dpi - 2){swapParticle(i, j, 1, 1, Type.sand, Type.void)}
 }
 
-function swapParticle(i, j, offsetX, offsetY){
-  grid[i+offsetX][j+offsetY] = Type.current; 
-  grid[i][j] = Type.void;
+function water(i, j){
+  if (grid[i][j+1] == Type.void){swapParticle(i, j, 0, 1, Type.water, Type.void)}
+  else if (grid[i+1][j] == Type.void && i != canvasWidth / dpi - 2){swapParticle(i, j, 1, 0, Type.water, Type.void)}
+  else if (grid[i-1][j] == Type.void && i != 1){swapParticle(i, j, -1, 0, Type.water, Type.void)}
 }
 
-function boundary(i, j){
-  if (j < canvasHeight / dpi && i < canvasWidth / dpi && j >= 0 && i > 0)
-    return 1;
-  else
-    return 0;
+function swapParticle(i, j, offsetX, offsetY, element, swap){
+  grid[i+offsetX][j+offsetY] = element;
+  grid[i][j] = swap;
 }
 
 function drawSim(){
 	for (let i = 0; i < canvasWidth / dpi; i++){
 		for (let j = 0; j < canvasHeight / dpi; j++){
-			if (grid[i][j] == 'sand'){fill(255, 196, 0)}
-      if (grid[i][j] == 'rock'){fill('grey')}
+			if (grid[i][j] == Type.sand){fill(255, 196, 0)}
+      if (grid[i][j] == Type.rock){fill('grey')}
+      if (grid[i][j] == Type.water){fill(212, 241, 249)}
     	if (grid[i][j] != Type.void){rect(i * dpi, j * dpi, dpi, dpi);}
     }
   }
 }
 
 function newArray(){
-  var grid = new Array(canvasWidth / dpi);
+  grid = new Array(canvasWidth / dpi);
   for ( i = 0; i < canvasWidth / dpi; i++)
     grid[i] = new Array(canvasHeight / dpi).fill(Type.void);
-  return grid;
 }
 
-function update(){
+function updateUI(){
   selColor = document.getElementById("brushColor").value;
   brushSize = document.getElementById("brushSlider").value;
   Type.current = document.getElementById("element").value;
   document.getElementById("framerate").innerHTML = round(frameRate());
   document.getElementById("xpos").innerHTML = round(pos.x / dpi);
   document.getElementById("ypos").innerHTML = round(pos.y / dpi);
+  document.getElementById("type").innerHTML = Type.current;
 }
 
 function clearGrid(){
