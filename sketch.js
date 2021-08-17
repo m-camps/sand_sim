b4394333698ba227c84e48b4ec5c05ecdf21f20c
Max <maxi.camps@gmail.com>
2021-08-17 11:59:21 +0200

    Working stone and sand

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var canvasWidth = 800;
 var canvasHeight = 800;
-var pxWidth = 10;
+var dpi = 5;
 var Type = {
   sand: 'sand',
   water: 'water',
@@ -19,8 +19,8 @@ function setup() {
 function draw() {
   background(100);
   fill(150);
-  rect(pxWidth, pxWidth, canvasWidth - (2*pxWidth), canvasHeight - (2*pxWidth));
-  pos = createVector(pmouseX - pmouseX % pxWidth, pmouseY - pmouseY % pxWidth);
+  rect(dpi, dpi, canvasWidth - (2*dpi), canvasHeight - (2*dpi));
+  pos = createVector(pmouseX - pmouseX % dpi, pmouseY - pmouseY % dpi);
   if (pos.x < 0)
 	pos.x = 0;
   click();
@@ -32,70 +32,72 @@ function draw() {
 
 function click(){
   if (mouseIsPressed){
-    if (mouseButton === LEFT && pos.x < canvasWidth - pxWidth&& pos.y < canvasHeight - pxWidth && pos.x > 0 && pos.y > 0){
-      grid[pos.x / pxWidth][pos.y / pxWidth] = Type.current;
+    if (mouseButton === LEFT && pos.x < canvasWidth - dpi&& pos.y < canvasHeight - dpi && pos.x > 0 && pos.y > 0){
+      grid[pos.x / dpi][pos.y / dpi] = Type.current;
     }
   }
 }
 
 function sim(){
-  for (let i = 0; i < canvasWidth / pxWidth; i++){
-    for (let j = 0; j < canvasHeight / pxWidth; j++){
+  for (let i = 0; i < canvasWidth / dpi; i++){
+    for (let j = 0; j < canvasHeight / dpi; j++){
       if (grid[i][j] != Type.void){
-		if (Type.current == Type.sand){sand(i, j);}
+		    sand(i, j);
       }
     }
   }
 }
 
 function revSim(){
-  for (let i = canvasWidth / pxWidth - 2; i > 0; i--){
-    for (let j = canvasHeight / pxWidth - 2; j > 0; j--){
+  for (let i = canvasWidth / dpi - 2; i > 0; i--){
+    for (let j = canvasHeight / dpi - 3; j > 0; j--){
       if (grid[i][j] != Type.void){
-        if (Type.current == Type.sand){sand(i, j);}
-		if (Type.current == Type.rock){rock(i, j)};
+        if (grid[i][j] == Type.sand){sand(i, j);}
+		    if (grid[i][j] == Type.rock){rock(i, j)};
       }
     }
   }
 }
+
 function rock(i, j){
-	swapParticle(i, j, 0, 0);
+	// swapParticle(i, j, 0, 0);
+  return  ;
 }
+
 function sand(i, j){
   if (grid[i][j+1] == Type.void){swapParticle(i, j, 0, 1)}
-  else if (grid[i][j+1] != Type.void && boundary(i, j, 0) == 1){
+  else if (grid[i][j+1] != Type.void && boundary(i, j) == 1){
     if (grid[i-1][j+1] == Type.void && i != 1){swapParticle(i, j, -1, 1)}
-    else if (grid[i+1][j+1] == Type.void && i !=  - 4){swapParticle(i, j, 1, 1)}
+    else if (grid[i+1][j+1] == Type.void && i != canvasWidth / dpi - 2){swapParticle(i, j, 1, 1)}
   }
-  else if (grid[i][j+1] == Type.sand && i == 0){swapParticle(i, j, 1, 0)}
-  else if (grid[i][j+1] == Type.sand && i == canvasWidth / pxWidth - 1){swapParticle(i, j, -1, 1)}
 }
 
 function swapParticle(i, j, offsetX, offsetY){
-  grid[i+offsetX][j+offsetY] = Type.current;
+  grid[i+offsetX][j+offsetY] = Type.current; 
   grid[i][j] = Type.void;
 }
 
-function boundary(i, j, z){
-  if (j + z < canvasHeight / pxWidth && i + z < canvasWidth / pxWidth && j - z >= 0 && i - z  > 0)
+function boundary(i, j){
+  if (j < canvasHeight / dpi && i < canvasWidth / dpi && j >= 0 && i > 0)
     return 1;
   else
     return 0;
 }
 
 function drawSim(){
-	for (let i = 0; i < canvasWidth / pxWidth; i++){
-		for (let j = 0; j < canvasHeight / pxWidth; j++){
+	for (let i = 0; i < canvasWidth / dpi; i++){
+		for (let j = 0; j < canvasHeight / dpi; j++){
 			if (grid[i][j] == 'sand'){fill(255, 196, 0)}
-    		if (grid[i][j] != Type.void){rect(i * pxWidth, j * pxWidth, pxWidth, pxWidth);}
+      if (grid[i][j] == 'rock'){fill('grey')}
+    	if (grid[i][j] != Type.void){rect(i * dpi, j * dpi, dpi, dpi);}
     }
   }
 }
 
 function newArray(){
-  var grid = new Array(canvasWidth / pxWidth);
-  for ( i = 0; i < canvasWidth / pxWidth; i++)
-    grid[i] = new Array(canvasHeight / pxWidth).fill(Type.void);
+  var grid = new Array(canvasWidth / dpi);
+  for ( i = 0; i < canvasWidth / dpi; i++)
+    grid[i] = new Array(canvasHeight / dpi).fill(Type.void);
   return grid;
 }
 
@@ -104,8 +106,8 @@ function update(){
   brushSize = document.getElementById("brushSlider").value;
   Type.current = document.getElementById("element").value;
   document.getElementById("framerate").innerHTML = round(frameRate());
-  document.getElementById("xpos").innerHTML = round(pos.x / pxWidth);
-  document.getElementById("ypos").innerHTML = round(pos.y / pxWidth);
+  document.getElementById("xpos").innerHTML = round(pos.x / dpi);
+  document.getElementById("ypos").innerHTML = round(pos.y / dpi);
 }
 
 function clearGrid(){
@@ -113,7 +115,8 @@ function clearGrid(){
 }
 
 function  printButton(){
-  console.log(Type.current);
-  console.log(Type.rock)
-  console.log(Type.current == Type.rock)
+  // console.log(Type.current);
+  // console.log(Type.rock)
+  // console.log(Type.current == Type.rock)
+  console.log(grid);
 }
